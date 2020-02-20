9d318b5a31192c42e004f2814694c221f978fae0
Maxime VISONNEAU <maxime.visonneau@gmail.com>
2020-02-20 13:13:01 +0000

    Added tests for the env provider

--- lib/providers/env/env_test.go
+++ lib/providers/env/env_test.go
@@ -1,3 +1,20 @@
 package env
 
-// TODO: Implement !
+import (
+	"os"
+	"testing"
+
+	"github.com/mvisonneau/go-helpers/test"
+	"github.com/mvisonneau/tfcw/lib/schemas"
+)
+
+func TestGetValue(t *testing.T) {
+	os.Setenv("TEST_ENV", "foo")
+
+	c := &Client{}
+	e := &schemas.Env{
+		Variable: "TEST_ENV",
+	}
+
+	test.Expect(t, c.GetValue(e), "foo")
+}
