mvisonneau/tfcw

commit cbbad7972b7927d9081d5b8b36b90383113e9099

Validate there is no pending run on the workspace before attempting to create a new one. fixes #9

cmd/run.go
@@ -21,6 +21,17 @@ func RunCreate(ctx *cli.Context) (int, error) {
 		return 1, err
 	}
 
+	if !ctx.Bool("ignore-pending-runs") {
+		runID, err := c.GetWorkspaceCurrentRunID(w)
+		if err != nil {
+			return 1, err
+		}
+
+		if runID != "" {
+			return 1, fmt.Errorf("there is already a run (%s) pending on your workspace (%s), exiting", runID, w.ID)
+		}
+	}
+
 	switch ctx.String("render-type") {
 	case "tfc":
 		err = c.RenderVariablesOnTFC(cfg, w, false, ctx.Bool("ignore-ttls"))
@@ -60,9 +71,14 @@ func RunApprove(ctx *cli.Context) (int, error) {
 		return 1, err
 	}
 
+	w, err := c.GetWorkspace(cfg.Runtime.TFC.Organization, cfg.Runtime.TFC.Workspace)
+	if err != nil {
+		return 1, err
+	}
+
 	runID := ctx.Args().Get(0)
 	if ctx.Bool("current") {
-		runID, err = c.GetWorkspaceCurrentRunID(cfg)
+		runID, err = c.GetWorkspaceCurrentRunID(w)
 		if err != nil {
 			return 1, err
 		}
@@ -82,9 +98,14 @@ func RunDiscard(ctx *cli.Context) (int, error) {
 		return 1, err
 	}
 
+	w, err := c.GetWorkspace(cfg.Runtime.TFC.Organization, cfg.Runtime.TFC.Workspace)
+	if err != nil {
+		return 1, err
+	}
+
 	runID := ctx.Args().Get(0)
 	if ctx.Bool("current") {
-		runID, err = c.GetWorkspaceCurrentRunID(cfg)
+		runID, err = c.GetWorkspaceCurrentRunID(w)
 		if err != nil {
 			return 1, err
 		}

cli/flags.go
@@ -14,16 +14,20 @@ var runCreate = []cli.Flag{
 		Usage: "automatically approve the run once planned",
 	},
 	cli.BoolFlag{
+		Name:  "ignore-pending-runs",
+		Usage: "it will create the run even if there is already one or more run(s) in the workspace queue",
+	},
+	cli.BoolFlag{
 		Name:  "no-prompt",
 		Usage: "will not prompt for approval once planned",
-	},
-	cli.DurationFlag{
-		Name:  "start-timeout,t",
-		Usage: "time to wait for the plan to start (set to 0 to disable, it is the default)",
 	},
 	cli.StringFlag{
 		Name:  "output,o",
 		Usage: "file on which to write the run ID",
+	},
+	cli.DurationFlag{
+		Name:  "start-timeout,t",
+		Usage: "time to wait for the plan to start (set to 0 to disable, it is the default)",
 	},
 }
 

cmd/workspace.go
@@ -27,7 +27,12 @@ func WorkspaceCurrentRunID(ctx *cli.Context) (int, error) {
 		return 1, err
 	}
 
-	runID, err := c.GetWorkspaceCurrentRunID(cfg)
+	w, err := c.GetWorkspace(cfg.Runtime.TFC.Organization, cfg.Runtime.TFC.Workspace)
+	if err != nil {
+		return 1, err
+	}
+
+	runID, err := c.GetWorkspaceCurrentRunID(w)
 	if err != nil {
 		return 1, err
 	}

lib/client/workspaces.go
@@ -168,18 +168,13 @@ func (c *Client) GetWorkspaceStatus(cfg *schemas.Config) error {
 }
 
 // GetWorkspaceCurrentRunID returns the status of the configured workspace
-func (c *Client) GetWorkspaceCurrentRunID(cfg *schemas.Config) (string, error) {
-	w, err := c.GetWorkspace(cfg.Runtime.TFC.Organization, cfg.Runtime.TFC.Workspace)
-	if err != nil {
-		return "", err
-	}
-
+func (c *Client) GetWorkspaceCurrentRunID(w *tfc.Workspace) (string, error) {
 	if w.Locked {
-		log.Debugf("Workspace %s is currently locked by run ID '%s'\n", cfg.Runtime.TFC.Workspace, w.CurrentRun.ID)
+		log.Debugf("Workspace %s is currently locked by run ID '%s'\n", w.ID, w.CurrentRun.ID)
 		return w.CurrentRun.ID, nil
 	}
 
-	return "", fmt.Errorf("workspace %s is currently idle", cfg.Runtime.TFC.Workspace)
+	return "", fmt.Errorf("workspace %s is currently idle", w.ID)
 }
 
 func (c *Client) updateSSHKey(w *tfc.Workspace, sshKeyName string) error {

commit e7a0db969a04f099e81ff4132562bccac7c236f3

fixed a bug on run creation introduced in cbbad79

cmd/run.go
@@ -22,12 +22,7 @@ func RunCreate(ctx *cli.Context) (int, error) {
 	}
 
 	if !ctx.Bool("ignore-pending-runs") {
-		runID, err := c.GetWorkspaceCurrentRunID(w)
-		if err != nil {
-			return 1, err
-		}
-
-		if runID != "" {
+		if runID, _ := c.GetWorkspaceCurrentRunID(w); runID != "" {
 			return 1, fmt.Errorf("there is already a run (%s) pending on your workspace (%s), exiting", runID, w.ID)
 		}
 	}